5ebc34dbf73bee380158d4ba58b6eaaf74a1f50b
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-02-04 22:40:13 +0530

    reversecd c:\JAVA\String\ ; if (True) { javac reverse.java } ; if (True) { java reverse }

---
+++ String/reverse.java
@@ -0,0 +1,18 @@
+public class reverse{
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder("preet bhingradiya");
+        // sb.reverse();
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front=i;
+            int back=sb.length()-1-i;  //5-1-0 = 4
+
+            char frontChar=sb.charAt(front);
+            char backChar=sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        System.out.println(sb);
+    }
+}
