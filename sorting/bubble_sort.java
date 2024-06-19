649957dbf17efe72e5c39d7c7ba64097ea650d78
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-06-19 21:43:48 +0530

    bubble sort

--- sorting/bubble_sort.java
+++ sorting/bubble_sort.java
@@ -11,6 +11,7 @@ public class bubble_sort {
 
     public static void main(String[] args){
         int[] arr={7,8,3,2,1};
+        boolean swapped ; //if the array is sorted this time
 
         for(int i=0;i<arr.length-1;i++){
            for(int j=i+1;j<arr.length;j++){
@@ -19,15 +20,19 @@ public class bubble_sort {
                     int temp=arr[i];
                     arr[i]=arr[j];
                     arr[j]=temp;
-                    
+                    swapped = true;
+
                     // without third variable
                     // arr[i]=arr[i]+arr[j];
                     // arr[j]=arr[i]-arr[j];
                     // arr[i]=arr[i]-arr[j];
                 }
            }
+
+           if(!swapped) break; //it menas swapped is false so no any first time swapp any value so it can swapp
         }        
         printArray(arr);
+        
         // for(int i=0;i<arr.length-1;i++){
         //     for(int j=0;j<arr.length-i-1;j++){
         //          if(arr[j]>arr[j+1]){
--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -8,19 +8,48 @@ public class selection_sort {
         }
         System.out.println();
     }
+
+
     public static void main(String[] args){
         int[] arr={7,8,1,3,2};
-        for(int i=0;i<arr.length-1;i++){
-            int minposi=i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[minposi]>arr[j]){
-                    minposi=j;
-                }
-            }
-            int val=arr[minposi];
-            arr[minposi]=arr[i];
-            arr[i]=val;
-        }
+        // for(int i=0;i<arr.length-1;i++){
+        //     int minposi=i;
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[minposi]>arr[j]){
+        //             minposi=j;
+        //         }
+        //     }
+        //     int val=arr[minposi];
+        //     arr[minposi]=arr[i];
+        //     arr[i]=val;
+        // }
+        selection(arr);
         printArray(arr);
+    }
+
+    public static void selection(int[] arr){
+        for(int i=0;i<arr.length;i++){
+
+            int last = arr.length-i-1;
+            int maxIndex = getFindMaxIndex(arr,0,last);
+            swap(arr, maxIndex , last); //swap every time max element int to last
+        }
+    }
+
+    public static void swap(int[] arr , int start ,int end){
+        arr[start] = arr[start] + arr[end];
+        arr[end] = arr[start] - arr[end];
+        arr[start] = arr[start] - arr[end];
+    }
+
+    public static int getFindMaxIndex(int[] arr , int start ,int end){
+        int max = arr[start];
+
+        for (int i = start; i < end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
     }
 }
