53c320bcdd511de969d44a0d000573c5e638ee70
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-07-06 22:09:53 +0530

    Akra bassi recursion formula

--- Recursion/Fibonaci_series.java
+++ Recursion/Fibonaci_series.java
@@ -1,14 +1,9 @@
+// Mathemic formula of Nth fibonacci is 1 / sqrt(5) * (1 + sqrt(5) / 2)power(n) - 1 / sqrt(5)* (1- sqrt(5) / 2) power(n)
+//common is 1 / sqrt(5)
 public class Fibonaci_series {
-    // fibonaci = 0,1,1,2,3,5,8,13....
-    // 5th position is 5
-    public static int fibonaciNum(int num) {
-
-        if (num == 0 || num==1)
-            return num;
-        int finalNumber = fibonaciNum(num - 1) + fibonaciNum(num - 2);
-        return finalNumber;
-    }
-    /*fibonaciNum(num - 1)  mate
+     /*fibonaci = 0,1,1,2,3,5,8,13....
+    5th position is 5
+       fibonaciNum(num - 1)  mate
      * 5
      * 4
      * 3
@@ -16,11 +11,27 @@ public class Fibonaci_series {
      * 1  return 1  1 per(fibonaciNum(num - 2))
      * 1 +  0 = 1 give to top 
      */
+    public static int fibonaciNum(int num) {
+
+        if (num == 0 || num==1)
+            return num;
+        int finalNumber = fibonaciNum(num - 1) + fibonaciNum(num - 2);
+        return finalNumber;
+    }
+
+    public static int fiboFormula(int n ){
+        // Math.pow(((1 + Math.sqrt(5)) / 2) , n) 
+        // Math.pow(((1 - Math.sqrt(5)) / 2) , n)
+        // (Math.pow(((1 + Math.sqrt(5)) / 2) , n) - Math.pow(((1 - Math.sqrt(5)) / 2) , n)) 
+        return (int) ((Math.pow(((1 + Math.sqrt(5)) / 2) , n) - Math.pow(((1 - Math.sqrt(5)) / 2) , n)) / Math.sqrt(5));
+    }
+
     public static void main(String[] args) {
         int num = 7;
         System.out.println(num + "th of fibonnacci is :- " + fibonaciNum(num));
-        for (int i = 0; i < num; i++) {
-            System.out.print(fibonaciNum(i) + " ");
+        for (int i = 0; i < 50; i++) {
+            // System.out.print(fibonaciNum(i) + " ");
+            System.out.print(fiboFormula(i) + " ");
         }
     }
 }
