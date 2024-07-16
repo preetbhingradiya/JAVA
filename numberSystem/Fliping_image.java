125dd18ccc24564bddac89160c347615b28f7b8e
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-07-16 21:51:22 +0530

    flip-image

--- numberSystem/Fliping_image.java
+++ numberSystem/Fliping_image.java
@@ -3,16 +3,18 @@ import java.util.Arrays;
 class Fliping_image {
 
     public static void main(String[] args) {
-        int[][] image = {{1,1,0},{1,0,1},{0,0,0}};
-        System.out.println(Arrays.toString(flipAndInvertImage(image)));
+        int[][] image = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+        System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
     }
+
     public static int[][] flipAndInvertImage(int[][] image) {
-       
-        for(int[] row : image){
-            for(int i = 0 ; i < (image[0].length + 1)/2 ; i++){ 
-                int temp = row[i] ^ 1;
-                row[i] = row[image[0].length - i -1]^1;
-                row[image[0].length - i -1] = temp;
+        for (int[] row : image) {
+            int n = row.length;
+            for (int i = 0; i < (n + 1) / 2; i++) {
+                // Flip and invert simultaneously
+                int temp = row[i] ^ 1; // Invert first element
+                row[i] = row[n - i - 1] ^ 1; // Invert the opposite element
+                row[n - i - 1] = temp; // Set the opposite element
             }
         }
         return image;
