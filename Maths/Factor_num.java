4fd227baebe10c1f80db2412e78f8c0c53d26503
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-07-19 21:56:20 +0530

    factor

--- Maths/Factor_num.java
+++ Maths/Factor_num.java
@@ -1,14 +1,48 @@
+import java.util.ArrayList;
+
 public class Factor_num {
     public static void main(String[] args) {
-        int n =20;
-        factorls(n);
+        int n = 20;
+        factorls3(n);
     }
 
-    static void factorls(int n){
-        for (int i = 1; i <=n ; i++) {
-            if(n % i == 0){
+    static void factorls(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
                 System.out.print(i + " ");
             }
+        }
+    }
+
+    public static void factorls2(int n) {
+        for (int i = 1; i <= Math.sqrt(n); i++) { // here 20%2 = 10 (2*10) so wh check the 20%10 = 2 (10*2) wo stil
+                                                  // check the sqrt of n
+            if (n % i == 0) {
+                if (n / i == i) { // manage duplicate if the nummber is 36 so not print two time 6
+                    System.out.println(i + " ");
+                } else {
+                    System.out.print(i + " " + n / i + " ");// here i asc and n/i is desc
+                }
+            }
+        }
+    }
+
+    // now sort in asending order
+    public static void factorls3(int n){
+        ArrayList<Integer> list=  new  ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) { 
+            if(n%i==0){
+                if(n/i == i){
+                    System.out.print(i + " ");
+                }else{
+                    System.out.print(i + " ");  
+                    list.add(n/i);
+                }
+            }
+        }
+
+        for (int j = list.size()-1; j >= 0; j--) {
+            System.out.print(list.get(j) + " ");
         }
     }
 }
--- Maths/Sieve_prime.java
+++ Maths/Sieve_prime.java
@@ -2,21 +2,21 @@
 public class Sieve_prime {
     public static void main(String[] args) {
         int n = 20;
-        boolean [] prime = new boolean[n+1];// by defoalte all the element in boolean is false
+        boolean[] prime = new boolean[n + 1];// by defoalte all the element in boolean is false
         seive(n, prime);
-    }   
-    
-    public static void seive(int n, boolean[] prime){
-        for (int i = 2; i*i <=n; i++) {  //only check the n squer here 20 = 4.47 something 4*4
-            if(!prime[i]){ //is number is false it measn number is prime
-                for (int j = i*2; j <=n; j+=i) {  //if the 2 is prime so all the double of 2 is not prime like 4,6,8,10,12..
-                    prime[j] = true; //its not prime
+    }
+
+    public static void seive(int n, boolean[] prime) {
+        for (int i = 2; i <= Math.sqrt(n); i++) { // only check the n squer here 20 = 4.47 something 4*4
+            if (!prime[i]) { // is number is false it measn number is prime
+                for (int j = i * 2; j <= n; j += i) { // if the 2 is prime so all the double of 2 is not prime like
+                                                      // 4,6,8,10,12..
+                    prime[j] = true; // its not prime
                 }
             }
         }
 
-
-        for (int i = 2; i <=n ; i++) {
+        for (int i = 2; i <= n; i++) {
             if (!prime[i]) {
                 System.out.print(i + " ");
             }
