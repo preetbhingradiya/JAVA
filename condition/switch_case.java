b7062b603198cc33da92f14c8d47c57d605cdfe3
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-08-20 13:11:42 +0530

    new switch-case

--- condition/switch_case.java
+++ condition/switch_case.java
@@ -5,18 +5,29 @@ public class switch_case {
         Scanner value = new Scanner(System.in);
         int tepm = value.nextInt();
 
-        switch (tepm) {
-            case 1:
-                System.out.println("Kem Cho");
-                break;
-            case 2:
-                System.out.println("Hello");
-                break;
-            case 3:
-                System.out.println("नमस्ते");
-                break;
-            default:
-                System.out.println("Buna ziua");
-        }
+        // switch (tepm) {
+        //     case 1:
+        //         System.out.println("Kem Cho");
+        //         break;
+        //     case 2:
+        //         System.out.println("Hello");
+        //         break;
+        //     case 3:
+        //         System.out.println("नमस्ते");
+        //         break;
+        //     default:
+        //         System.out.println("Buna ziua");
+        // }
+
+        //In New Switch Statment 
+        String result = "";
+        result = switch (tepm) {
+            case 1 -> "Kem Cho";
+            case 2 -> "Hello";
+            case 3 -> "नमस्ते";   
+            default -> "Buna ziua";
+        };
+
+        System.out.println(result);
     }
 }
